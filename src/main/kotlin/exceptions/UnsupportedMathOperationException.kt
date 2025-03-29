f4858ec19840f458dd4a7fc08c955e0127570c3e
Hugo Freire <hfreire@MacBook-Pro-de-Hugo.local>
2025-03-29 20:47:27 -0300

    initial commit

---
+++ src/main/kotlin/exceptions/UnsupportedMathOperationException.kt
@@ -0,0 +1,4 @@
+package exceptions
+
+class UnsupportedMathOperationException(ex: String?): RuntimeException(ex) {
+}
---
+++ src/main/kotlin/math/SimpleMath.kt
@@ -0,0 +1,14 @@
+package math
+
+import kotlin.math.sqrt
+import kotlin.time.Duration
+
+class SimpleMath {
+
+    fun sum(numberOne: Double, numberTwo: Double) = numberOne + numberTwo
+    fun subtraction(numberOne: Double, numberTwo: Double) = numberOne - numberTwo
+    fun multiplication(numberOne: Double, numberTwo: Double) = numberOne * numberTwo
+    fun division(numberOne: Double, numberTwo: Double) = numberOne / numberTwo
+    fun mean(numberOne: Double, numberTwo: Double) = (numberOne + numberTwo) / 2
+    fun squareRoot(number: Double) = sqrt(number)
+}
